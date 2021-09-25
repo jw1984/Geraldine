f130baf93c821ea07cc221df886a3c30cb51ad37
Jesse John Wyer <wyerjess888@gmail.com>
2021-09-25 10:12:57 -0700

    Add files via upload

--- Geraldine.php
+++ Geraldine.php
@@ -9,7 +9,7 @@
         <script src='jquery-2.1.3.min.js'></script>
 
 
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="myCSS.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<style>
 	html {  }
